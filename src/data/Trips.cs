e0cef5915fd35a4aae7e4cf15acf31e6a09cf3aa
tyler <tyler@desktop>
2019-09-15 12:42:30 -0600

    Add direction filter to trip query.

--- src/data/Trips.cs
+++ src/data/Trips.cs
@@ -10,27 +10,38 @@ namespace RtdData
 {
     public class Trip : Base
     {
+      // Route Name (FF7, 80L, etc)
       [Name("route_id")]
       public string RouteId { get; set; }
 
+      // WK = Weekday
+      // SA = Saturday
+      // SU = Sunday
+      // Holiday / Special
       [Name("service_id")]
       public string ServiceId { get; set; }
 
+      // Uniquie ID for the Route/Service
       [Name("trip_id")]
       public int Id { get; set; }
 
-      // [Name("trip_headsign")]
+      // [Name("trip_headsign")] // Text displayed on the Bus
       // public string Headsign { get; set; }
 
-      // [Name("direction_id")]
-      // public int DirectionId { get; set; }
+      // 0: Travel in one direction of your choice, such as outbound travel.
+      // 1: Travel in the opposite direction, such as inbound travel.
+      [Name("direction_id")]
+      public int DirectionId { get; set; }
 
       // [Name("block_id")]
       // public string BlockId { get; set; }
 
       private List<Trip> _trips = null;
+      
+      /// Load items from the csv file in prep for future calls
       public override async Task InitAsync(string tripsFile)
       {
+        await base.InitAsync(tripsFile);
         _initTask = Task.Run(() => {
           using (var reader = new StreamReader(tripsFile))
           using (var csv = new CsvReader(reader))
@@ -42,37 +53,50 @@ namespace RtdData
         return;
       }
 
-      public List<Trip> GetTripsByRoute(string routeId)
+      /// RouteIds are FF7, FF3, 80L, etc
+      public List<Trip> GetTripsByRoute(string routeId, int? direction = null)
       {
         base.WaitForLoading();
         var matchingTrips = 
           _trips.Where(trip =>
                       trip.RouteId == routeId)
-                .Select(s => s)
-                .ToList();
+                .OrderBy(t => t.Id)
+                .Select(s => s);
         
-        // matchingTrips
+        if(direction != null)
+        {
+          matchingTrips = matchingTrips.Where(t => t.DirectionId == direction);
+        }
+        // matchingTrips.ToList()
         //   .ForEach(t =>
         //     Console.WriteLine($"TripId: {t.Id} ServiceId: {t.ServiceId} Route: {routeId}")
         //   );
-        return matchingTrips;
+        return matchingTrips.ToList();
       }
 
-      public List<Trip> GetTripsByRoute(List<string> routeIds)
+      // Get the Trip information for the provided routes
+      // Routes are FF7, 80L, etc
+      public List<Trip> GetTripsByRoute(List<string> routeIds, int? direction = null)
       {
         base.WaitForLoading();
 
         var matchingTrips = 
           _trips.Where(trip =>
                       routeIds.Contains(trip.RouteId) )
-                .Select(s => s)
-                .ToList();
+                .OrderBy(t => t.Id)
+                .Select(s => s);
+
+        if(direction != null)
+        {
+          matchingTrips = matchingTrips.Where(t => t.DirectionId == direction);
+        }
         
         // matchingTrips
+        //   .ToList()
         //   .ForEach(t =>
-        //     Console.WriteLine($"TripId: {t.Id} ServiceId: {t.ServiceId} Route: {t.RouteId}")
+        //     Console.WriteLine($"TripId: {t.Id} ServiceId: {t.ServiceId} Route: {t.RouteId} Dir: {t.DirectionId}")
         //   );
-        return matchingTrips;
+        return matchingTrips.ToList();
       }
     }
 }
