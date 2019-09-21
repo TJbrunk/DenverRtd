565fa39beb0684a37d6bb6406613c3b3802d8ec0
tyler <tyler@desktop>
2019-09-20 23:24:57 -0600

    Clean up code.

--- src/Rtd/Trips.cs
+++ src/Rtd/Trips.cs
@@ -8,46 +8,9 @@ using RtdData.Entities;
 
 namespace RtdPlanner
 {
-    public class Trip : Base
+    public class Trip
     {
-/*      private List<Trip> _trips = null;
-      
-      /// Load items from the csv file in prep for future calls
-      public override async Task InitAsync(string tripsFile)
-      {
-        await base.InitAsync(tripsFile);
-        _initTask = Task.Run(() => {
-          using (var reader = new StreamReader(tripsFile))
-          using (var csv = new CsvReader(reader))
-          {
-            _trips = csv.GetRecords<Trip>().ToList();
-          }
-        });
-        await _initTask;
-        return;
-      }
 
-      /// RouteIds are FF7, FF3, 80L, etc
-      public List<Trip> GetTripsByRoute(string routeId, int? direction = null)
-      {
-        base.WaitForLoading();
-        var matchingTrips = 
-          _trips.Where(trip =>
-                      trip.RouteId == routeId)
-                .OrderBy(t => t.Id)
-                .Select(s => s);
-        
-        if(direction != null)
-        {
-          matchingTrips = matchingTrips.Where(t => t.DirectionId == direction);
-        }
-        // matchingTrips.ToList()
-        //   .ForEach(t =>
-        //     Console.WriteLine($"TripId: {t.Id} ServiceId: {t.ServiceId} Route: {routeId}")
-        //   );
-        return matchingTrips.ToList();
-      }
-*/
       // Get the Trip information for the provided routes
       // Routes are FF7, 80L, etc
       public static List<TripEntity> GetTripsByRoute(List<string> routeIds, int? direction = null)
